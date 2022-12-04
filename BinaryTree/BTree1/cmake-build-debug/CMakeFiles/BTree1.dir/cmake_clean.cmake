94b5b3781781f6778ab6189e5c518f018ab5f9a4
1900MER <48175916+1900MER@users.noreply.github.com>
2022-12-04 22:19:16 +0100

    Btree

---
+++ BinaryTree/BTree1/cmake-build-debug/CMakeFiles/BTree1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BTree1"
+  "BTree1.pdb"
+  "CMakeFiles/BTree1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BTree1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
